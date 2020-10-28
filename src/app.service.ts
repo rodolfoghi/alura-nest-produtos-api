83f01f44715394784c6db34b8dd197a58a86a593
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-10-28 18:08:13 -0300

    Video 5.1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,5 +3,11 @@ import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class AppService {
-  constructor(private sequelize: Sequelize) {}
+  constructor(private sequelize: Sequelize) {
+    this.sincronizaBancoDados();
+  }
+
+  async sincronizaBancoDados() {
+    await this.sequelize.sync();
+  }
 }
